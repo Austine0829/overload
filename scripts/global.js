7509b6775651a9ad4220caea78af0d7a6147fdf1
Austine0829 <audreytuazon29@gmail.com>
2025-08-07 01:31:30 +0800

    [update] added show and hide for the cart

--- scripts/global.js
+++ scripts/global.js
@@ -1,10 +1,18 @@
+const cart = document.querySelector('.cart-wrapper');
+
 document.querySelector('.nav-menu').addEventListener('click', () => {
     const navLinks = document.querySelector('.nav-links-wrapper ul');
     navLinks.classList.toggle('show-nav-menu');
 });
 
-// TODO: design needs fixing
 document.querySelector('.cart-icon').addEventListener('click', () => {
-    const cart = document.querySelector('.cart-wrapper');
+    cart.classList.remove('hide-cart');
+    document.body.classList.toggle('no-scroll');
     cart.classList.toggle('show-cart');
+});
+
+document.querySelector('.cart-close').addEventListener('click', () => {
+    cart.classList.remove('show-cart');
+    document.body.classList.remove('no-scroll');
+    cart.classList.toggle('hide-cart');
 });
