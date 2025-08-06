f3cac539835ba510439ed7a6b98694b20f514d5c
Austine0829 <audreytuazon29@gmail.com>
2025-08-07 00:45:05 +0800

    [add] fetch methods for home items

---
+++ scripts/home.js
@@ -0,0 +1,74 @@
+const cardsScrollableContainer = document.querySelector('.cards-scrollable-container');
+const leftScroll = document.querySelector('.card-scroll-button.left');
+const rightScroll = document.querySelector('.card-scroll-button.right');
+
+async function getAllNewProduct() {
+    try {
+        const response = await fetch("/tshirts.json");
+
+        if (!response.ok) return;
+
+        const data = await response.json();
+        for (const item of data) {
+            if (item.status === 'new') {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+
+                card.innerHTML = `<img src="${item.img}">
+                                 <div class="card-name-and-price-wrapper">
+                                    <label>${item.name}</label>
+                                    <label>₱${item.price}</label>
+                                 </div>
+                                 <div class="card-color-description-wrapper">
+                                    <label>${item.color}</label>
+                                 </div>`;
+
+                cardsScrollableContainer.appendChild(card);
+            }
+        }
+
+    } catch (error) {
+        console.log('Error Details: ', error);
+    }
+}
+
+leftScroll.addEventListener('click', () => {
+    cardsScrollableContainer.scrollBy({ left: -1, behavior: 'smooth' });
+});
+
+rightScroll.addEventListener('click', () => {
+    cardsScrollableContainer.scrollBy({ left: 1, behavior: 'smooth' });
+});
+
+async function getAllProduct() {
+    try {
+        const response = await fetch("/tshirts.json");
+
+        if (!response.ok) return;
+
+        cardsGridContainer = document.querySelector('.cards-grid-container');
+
+        const data = await response.json();
+        for (const item of data) {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card mobile-card');
+
+            card.innerHTML = `<img src="${item.img}">
+                                <div class="card-name-and-price-wrapper">
+                                <label>${item.name}</label>
+                                <label>₱${item.price}</label>
+                              </div>
+                              <div class="card-color-description-wrapper">
+                                <label>${item.color}</label>
+                              </div>`;
+
+            cardsGridContainer.appendChild(card);
+        }
+
+    } catch (error) {
+        console.log('Error Details: ', error);
+    }
+}
+
+getAllNewProduct();
+getAllProduct();
